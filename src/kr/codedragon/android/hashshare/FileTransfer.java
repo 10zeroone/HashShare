361c1c3264673b1f9dc1be07836d9546ed045a57
10zeroone <to2share@gmail.com>
2014-05-22 09:52:33 +0900

    publish version commit

---
+++ src/kr/codedragon/android/hashshare/FileTransfer.java
@@ -0,0 +1,64 @@
+package kr.codedragon.android.hashshare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FileTransfer extends Activity implements OnClickListener{
+
+	Button btn_Email;
+	TextView tvState;
+	
+	Uri URI = null;
+	String path =null;
+	String hashMD5 = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//화면에 무엇을 보여줄 지를 결정하는 함수
+		setContentView(R.layout.activity_filetransfer);
+
+		tvState = (TextView)findViewById(R.id.tvState);		
+
+		//데이터 가져오기
+		Intent intent = getIntent();
+		Bundle bundle = intent.getExtras();
+
+		if(bundle== null){	//데이터가 보관할 수 있는 번들객체가 null이면 데이터가 없음을 의미		
+			tvState.append("\n\n 전달받은 데이터가 없습니다!");
+		}else{
+			path = bundle.getString("msg");
+			hashMD5 = bundle.getString("hashMD5");
+			
+			URI = Uri.parse("file://" + path);
+			tvState.append("\n\n" + path);
+		}
+		
+		btn_Email = (Button)findViewById(R.id.btn_Email);
+		btn_Email.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v == btn_Email){
+			Intent email = new Intent(Intent.ACTION_SEND);
+            email.putExtra(Intent.EXTRA_EMAIL, "");
+            email.putExtra(Intent.EXTRA_SUBJECT, "파일 전달");
+            email.putExtra(Intent.EXTRA_TEXT, "파일 첨부합니다.\n MD5: " + hashMD5);
+            if (URI != null) {
+            	email.putExtra(Intent.EXTRA_STREAM, URI);
+			}
+            //email client 설정
+            email.setType("message/rfc822");
+
+            startActivity(Intent.createChooser(email, "메일을 보낼 프로그램 선택:"));
+		}
+	}
+}
